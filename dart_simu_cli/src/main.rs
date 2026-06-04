64f4d9018aa9c0a6dc2fa331c98c9d3a82037f42
Thejulfi <40205050+Thejulfi@users.noreply.github.com>
2026-06-04 22:16:01 +0200

    changed(dart_simu_cli/main): default websocket server url

--- dart_simu_cli/src/main.rs
+++ dart_simu_cli/src/main.rs
@@ -10,7 +10,7 @@ use url::Url;
 #[command(author, version, about = "Interactive CLI that can send messages over WebSocket")]
 struct Args {
     /// Server WebSocket URL
-    #[arg(short, long, default_value = "ws://127.0.0.1:8000/ws")]
+    #[arg(short, long, default_value = "ws://127.0.0.1:8080/ws/impacts")]
     url: String,
 }
 
